8869c08668cb8b45ec59325c03447ccdf1540fab
firussgit <firussmail@gmail.com>
2023-05-22 12:45:05 +0100

    Basic Rock Paper Scissors

---
+++ main.js
@@ -0,0 +1,35 @@
+let playerScore = 0;
+let aiScore = 0;
+
+function playRound(playerMove, aiMove) {
+    if (playerMove != "rock" && playerMove != "paper" && playerMove != "scissors") {
+        console.log("You didn't pick a valid weapon! You lose..");
+    } else if (aiMove === playerMove) {
+        console.log("Draw.");
+    } else if ((playerMove === "paper" && aiMove === "rock") || (playerMove === "rock" && aiMove === "scissors") || (playerMove === "scissors" && aiMove === "paper")) {
+        console.log("Player win!");
+        playerScore++;
+    } else {
+        console.log("AI win!");
+        aiScore++;
+    }
+
+    console.log(`Player: ${playerScore} - AI: ${aiScore}`);
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const choices = ["rock", "paper", "scissors"];
+        const random = Math.floor(Math.random() * choices.length);
+
+        const playerMove = prompt("Choose your weapon: ").toLowerCase();
+        console.log(`Player: ${playerMove}`);
+
+        const aiMove = choices[random];
+        console.log(`AI: ${aiMove}`);
+
+        playRound(playerMove, aiMove);
+    }
+}
+
+game();
